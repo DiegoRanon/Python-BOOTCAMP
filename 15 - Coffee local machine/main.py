f91b6275478d5d3672083d235ffdb36c1d5fd79a
Diego Ranon <Mastercelis8891@gmail.com>
2024-03-18 10:05:09 -0400

    Initial Commit

---
+++ 15 - Coffee local machine/main.py
@@ -0,0 +1,44 @@
+# JSON 
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+# Variables
+profit = 0
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100
+}
+
+# Functions
+
+while(True):
+    choice = input("What would you like? (espresso/latte/cappuccino):")
+    if choice == "off":
+        break
+
+print("The machine is now off.")
+
